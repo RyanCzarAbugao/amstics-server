1c45f9c5e439b93719784bf303990d14b0c6c19a
Ryan Czar Abugao <121326971+RyanCzarAbugao@users.noreply.github.com>
2024-04-24 09:36:04 +0800

    test 7

--- src/server.ts
+++ src/server.ts
@@ -31,13 +31,15 @@ const corsOptions = {
 
 app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
 
-app.use(cors(corsOptions));
-
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://iridescent-truffle-570beb.netlify.app');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
   next();
 });
 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
