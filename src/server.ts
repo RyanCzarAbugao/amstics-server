bd0b6309c264c9091cc0755d354e485b0d7d4937
Ryan Czar Abugao <121326971+RyanCzarAbugao@users.noreply.github.com>
2024-04-24 09:15:30 +0800

    test 4

--- src/server.ts
+++ src/server.ts
@@ -29,12 +29,13 @@ const corsOptions = {
 
 app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
 
+app.use(cors(corsOptions));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
-app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
