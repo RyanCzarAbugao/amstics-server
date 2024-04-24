a51eaa2862d36ede7386f0a01f752465cfa61022
Ryan Czar Abugao <121326971+RyanCzarAbugao@users.noreply.github.com>
2024-04-24 08:03:54 +0800

    allow netlify to consume resources from the host of this node server

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,20 @@ const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'http://localhost';
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://app.netlify.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
